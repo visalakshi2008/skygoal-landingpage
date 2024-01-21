8c18ec04fc1bdb696508187cf937af27d798343b
visalakshi2008 <58606388+visalakshi2008@users.noreply.github.com>
2024-01-21 11:41:19 +0530

    Update CustomHeader.js

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -3,8 +3,6 @@ import React, { useState, useEffect } from 'react';
 import {
     HomeOutlined,
     InfoCircleOutlined,
-    MailOutlined,
-    UserAddOutlined,
     DollarOutlined,
     MenuOutlined,
     UserOutlined,
